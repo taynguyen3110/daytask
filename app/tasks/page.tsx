9fc3c8860e888b2e6a9b8c44b07c5a74f36d3a0d
taynguyen3110 <taynguyen3110@gmail.com>
2025-05-08 00:47:25 +1000

    fix: suspense TaskList component

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react"
 import { MainLayout } from "@/components/layouts/main-layout"
 import { TaskList } from "@/components/tasks/task-list"
 
 export default function TasksPage() {
   return (
     <MainLayout>
-      <TaskList />
+      <Suspense fallback={<div>Loading tasks...</div>}>
+        <TaskList />
+      </Suspense>
     </MainLayout>
   )
 }
