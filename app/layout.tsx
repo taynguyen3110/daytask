bbe57a8acdb466fc587040ae32f80b9435f84f74
taynguyen3110 <taynguyen3110@gmail.com>
2025-05-30 16:14:42 +1000

    feat: update favi icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,18 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { icons } from "lucide-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "DayTask",
   description: "A powerful yet simple task management application",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon-32x32.png",
+  },
 };
 
 export default function RootLayout({
